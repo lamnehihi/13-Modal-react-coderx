84358c678ba8728010ad62577cad356ee522cb4a
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-06-01 20:42:02 +0700

    Revert "13-Handling-action"
    This reverts commit cfc2aea7908f046c5fc4f76e9fd434fafed26010.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,22 @@
 import React, { Component } from "react";
-import "./App.css";
-import Modal from "./components/Modal";
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      isShow: true,
-    };
+  
+    }
   }
-
-  turn() {
-    this.setState({
-      isShow: !this.state.isShow,
-    });
-    console.log("change!" + this.state.isShow)
-  }
-
   render() {
-    const isShow = this.state.isShow;
     return (
       <div className="App">
-        <button onClick={this.turn.bind(this)}>Modal</button>
-        {
-          isShow && <Modal onClick={this.turn.bind(this)} isShow={isShow}/>
-        }
+      
       </div>
     );
   }
 }
 
 export default App;
+
