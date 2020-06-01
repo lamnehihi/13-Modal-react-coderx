cfc2aea7908f046c5fc4f76e9fd434fafed26010
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-06-01 20:39:26 +0700

    13-Handling-action

---
+++ src/components/Modal.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import "./Modal.css";
+
+class Modal extends Component {
+  render() {
+    const onClick = this.props.onClick;
+    return (
+      <div className="Modal">
+        <div className="header">
+          <h1>This is a model 1</h1>
+          <span>
+            <a href="#" onClick={onClick}>
+              x
+            </a>
+          </span>
+        </div>
+
+        <div className="body">
+          <p >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+            incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+            ea commodo consequat. Duis aute irure dolor in reprehenderit in
+            voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+            officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
+            amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+            nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+        </div>
+
+        <div className="btn">
+          <button onClick={onClick} className="accept">Accept</button>
+          <button onClick={onClick} className="decline">Decline</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
