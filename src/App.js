da5b1f88779f867e33ff970c6b1b4bd266302de3
Thanh Lam <thanhlamnguyenhuynh@gmail.com>
2020-06-01 20:45:02 +0700

    13-hadling-action

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
 import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Modal from "./components/Modal";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-  
-    }
+      isShow: true,
+    };
   }
+
+  turn() {
+    this.setState({
+      isShow: !this.state.isShow,
+    });
+    console.log("change!" + this.state.isShow)
+  }
+
   render() {
+    const isShow = this.state.isShow;
     return (
       <div className="App">
-      
+        <button onClick={this.turn.bind(this)}>Modal</button>
+        {
+          isShow && <Modal onClick={this.turn.bind(this)} isShow={isShow}/>
+        }
       </div>
     );
   }
 }
 
 export default App;
-
